e2fead01c983e3c042363cf9c93c4ca4de330381
darcythomas <github@darcy.io>
2014-01-03 17:05:42 +1300

    now with messages

--- jenkins-deploy.sh
+++ jenkins-deploy.sh
@@ -3,12 +3,10 @@
 
 echo Start deploy
 
-#./haywire.sh stop
-#./haywire.sh start
-
-
+echo Kill old
 pkill haywire_hello_w
 
+echo Start the new
 ( $PWD/builds/unix/debug/haywire_hello_world & ) 
 
 echo done
