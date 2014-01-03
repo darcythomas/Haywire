73e1098f65b419241c2301e6208e3c015626fa84
darcythomas <github@darcy.io>
2014-01-03 17:56:49 +1300

    Trying another way of running in the background

---
+++ PushToTestServer.ps1
@@ -0,0 +1,3 @@
+﻿cd C:\Users\darcythomas\Documents\GitHub\Haywire
+ git commit -a -m "push to test server, re base this later"
+ git push
--- jenkins-deploy.sh
+++ jenkins-deploy.sh
@@ -11,6 +11,6 @@ echo Kill old
 pkill haywire_hello_w
 
 echo Start the new
-( $PWD/builds/unix/debug/haywire_hello_world & ) 
+setsid $PWD/builds/unix/debug/haywire_hello_world 
 
 echo done
