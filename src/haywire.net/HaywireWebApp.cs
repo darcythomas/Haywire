e9b04625dc23136c4bae48f2e65c5e9e385b19f4
darcythomas <github@darcy.io>
2015-03-08 19:11:01 +1300

    Added .sln and Owin nuget package to haywire.net

---
+++ src/haywire.net/HaywireWebApp.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace haywire
+{
+   public static class HaywireWebApp
+    {
+        public static IDisposable Start(string url, Action<IAppBuilder> startup)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
