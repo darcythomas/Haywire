61058254bf9d30f03b76f5120af96e0f409d2f28
darcythomas <github@darcy.io>
2015-03-08 21:30:05 +1300

    brought in hello world code

--- src/haywire.net/HaywireWebApp.cs
+++ src/haywire.net/HaywireWebApp.cs
@@ -8,9 +8,52 @@ namespace haywire
 {
    public static class HaywireWebApp
     {
-        public static IDisposable Start(string url, Action<IAppBuilder> startup)
+
+        private static HaywireRequestCallback rootCallback;
+
+        public static IDisposable Start(String url, Action<IAppBuilder> startup)
         {
+            Uri cleanUri;
+
+            if (!Uri.TryCreate(url,UriKind.Absolute,out cleanUri))
+            {
+                throw new ArgumentException(String.Format("The url provided ({0}) is invalid."));
+            }
+
+            return Start(cleanUri, startup);
+        }
+
+        public static IDisposable Start(Uri url, Action<IAppBuilder> startup)
+        {
+            HaywireConfiguration config;
+            config.ListenAddress = url.Host;
+
+          
+                config.ListenPort = url.Port;
+          
+
+            var server = new HaywireServer(config);
+            rootCallback = new HaywireRequestCallback(GetRoot);
+            server.AddRoute("/", rootCallback);
+            server.StartAcceptingRequests();
+
             throw new NotImplementedException();
+        }
+
+        private static void GetRoot(HaywireRequest request, IntPtr response, IntPtr state)
+        {
+            var resp = new HaywireResponse(response);
+            resp.SetCode(StatusCodes.HTTP_STATUS_200);
+            resp.SetHeader("Content-Type", "text/html");
+            resp.SetHeader("Connection", "Keep-Alive");
+            resp.SetHeader("foo", "bar");
+            resp.SetBody("hello world");
+            resp.Send(ResponseComplete);
+        }
+
+        private static void ResponseComplete(IntPtr state)
+        {
+
         }
     }
 }
