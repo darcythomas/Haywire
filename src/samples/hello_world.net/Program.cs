e2f56ad2f78779eb52562340c84cbdf26aed1bbe
darcythomas <github@darcy.io>
2015-03-12 08:37:04 +1300

    failed attempt to use haywire OWIN in sample  app
    Having issues with loading the haywire-net dlls

--- src/samples/hello_world.net/Program.cs
+++ src/samples/hello_world.net/Program.cs
@@ -1,45 +1,71 @@
-﻿using System;
+﻿using Owin;
+using System;
+using haywire;
+using System.Diagnostics;
 
 namespace haywire.samples.helloworld
 {
     class Program
     {
-        private static HaywireRequestCallback rootCallback;
+        //private static HaywireRequestCallback rootCallback;
 
         static void Main(string[] args)
         {
-            HaywireConfiguration config;
-            config.ListenAddress = "0.0.0.0";
+           
+            string url = "http;//localhost:8080";
 
-            if (args.Length > 0)
+            HaywireWebApp app = new HaywireWebApp();
+            using (var q = app.Start(url, builder => builder.xyz()))
             {
-                config.ListenPort = int.Parse(args[0]);
-            }
-            else
-            {
-                config.ListenPort = 8000;
-            }
+                Debug.WriteLine("hi");
+            }     
 
-            var server = new HaywireServer(config);
-            rootCallback = new HaywireRequestCallback(GetRoot);
-            server.AddRoute("/", rootCallback);
-            server.StartAcceptingRequests();
+            //HaywireConfiguration config;
+            //config.ListenAddress = "0.0.0.0";
+
+            //if (args.Length > 0)
+            //{
+            //    config.ListenPort = int.Parse(args[0]);
+            //}
+            //else
+            //{
+            //    config.ListenPort = 8000;
+            //}
+
+            //var server = new HaywireServer(config);
+            //rootCallback = new HaywireRequestCallback(GetRoot);
+            //server.AddRoute("/", rootCallback);
+            //server.StartAcceptingRequests();
         }
 
-        private static void GetRoot(HaywireRequest request, IntPtr response, IntPtr state)
-        {
-            var resp = new HaywireResponse(response);
-            resp.SetCode(StatusCodes.HTTP_STATUS_200);
-            resp.SetHeader("Content-Type", "text/html");
-            resp.SetHeader("Connection", "Keep-Alive");
-            resp.SetHeader("foo", "bar");
-            resp.SetBody("hello world");
-            resp.Send(ResponseComplete);
-        }
 
-        private static void ResponseComplete(IntPtr state)
-        {
+       
+
+        //private static void GetRoot(HaywireRequest request, IntPtr response, IntPtr state)
+        //{
+        //    var resp = new HaywireResponse(response);
+        //    resp.SetCode(StatusCodes.HTTP_STATUS_200);
+        //    resp.SetHeader("Content-Type", "text/html");
+        //    resp.SetHeader("Connection", "Keep-Alive");
+        //    resp.SetHeader("foo", "bar");
+        //    resp.SetBody("hello world");
+        //    resp.Send(ResponseComplete);
+        //}
+
+        //private static void ResponseComplete(IntPtr state)
+        //{
             
+        //}
+    }
+    public static class Startup
+    {
+        public static void xyz(this IAppBuilder app)
+        {
+            app.UseHandler((request, res) =>
+            {
+                res.ContentType = "text/plain";
+                return res.WriteAsync("Hello, World!");
+            });
         }
     }
 }
