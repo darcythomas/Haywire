6e6ba041f83254e89dd42c440b8e10af95383ad5
darcythomas <github@darcy.io>
2015-03-12 08:35:17 +1300

    Minimal console app to test haywire running OWIN

---
+++ src/ConsoleApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using haywire;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8080";
+
+            HaywireWebApp app = new HaywireWebApp();
+            using (var q = app.Start(url, builder => builder.xyz()))
+            {
+                Debug.WriteLine("hi");
+                Console.ReadKey();
+            }
+        }
+
+       
+    }
+    public static class Startupx
+    {
+        public static void xyz(this IAppBuilder app)
+        {
+            app.UseHandler((request, res) =>
+            {
+                res.ContentType = "text/plain";
+                return res.WriteAsync("Hello, World!!!!");
+            });
+        }
+    }
+}
